46a35f68f5ef640bca845eab2b3e882e05666cae
Nathan Tsai <natesigh@gmail.com>
2021-03-07 04:52:40 -0800

    add save model code, clean for training

--- src/pinsage/model.py
+++ src/pinsage/model.py
@@ -1,37 +1,42 @@
+# built-in imports
 import pickle
 import argparse
+import os
+
+# third-party imports
 import numpy as np
+import pandas as pd
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader
 import torchtext
-import dgl
-import tqdm
+from tqdm import tqdm
 
+# local imports
 import layers
 import sampler as sampler_module
 import evaluation
 
 class PinSAGEModel(nn.Module):
     """Wrapper class for the PinSAGE model."""
-    def __init__(self, full_graph, ntype, textsets, hidden_dims, n_layers):
+    def __init__(self, full_graph, ntype, textset, hidden_dims, n_layers):
         """Constructor of PinSAGE model class.
 
         Args:
             full_graph (dgl.DGLGraph): bipartite user-item graph
             ntype (str): item node name
-            textsets (torchtext.data.Dataset): text features
+            textset (torchtext.data.Dataset): text features
             hidden_dims (int): dimension of hidden layer
             n_layers (int): number of hidden layers
         """
         super().__init__()
 
-        self.proj = layers.LinearProjector(full_graph, ntype, textsets, hidden_dims)
+        self.proj = layers.LinearProjector(full_graph, ntype, textset, hidden_dims)
         self.sage = layers.SAGENet(hidden_dims, n_layers)
         self.scorer = layers.ItemToItemScorer(full_graph, ntype)
 
     def forward(self, pos_graph, neg_graph, blocks):
-        """Forward propogation of PinSAGE model.
+        """Forward propagation of PinSAGE model.
 
         Args:
             pos_graph (dgl.DGLGraph): positive-connected graph
@@ -47,88 +52,119 @@ class PinSAGEModel(nn.Module):
         return (neg_score - pos_score + 1).clamp(min=0)
 
     def get_repr(self, blocks):
-        """TODO: Returns representation of ???"""
+        """Returns the embedded representation given block made from sampling neighboring nodes."""
         h_item = self.proj(blocks[0].srcdata)
-        h_item_dst = self.proj(blocks[-1].dstdata)
-        return h_item_dst + self.sage(blocks, h_item)
+        h_item_dst = self.proj(blocks[-1].dstdata) # node's own learnable embedding
+        return h_item_dst + self.sage(blocks, h_item) # embedding + GNN output
 
-def train(dataset, args):
+
+def train(dataset, model_cfg):
     """Creates and trains a PinSAGE model using Adam optimizer.
 
     Args:
-        dataset (dict): dictionary of preprocessed dataset features and information
-        args (argparse.ArgumentParser): model and data configuration arguments
+        dataset (dict[str,*]): dictionary of preprocessed dataset features and information
+        model_cfg (dict[str,str]): model configuration parameters
+
+    Returns:
+        A trained PinSAGE model.
     """
     g = dataset['train-graph'] # training graph
     val_matrix = dataset['val-matrix'].tocsr() # compressed sparse row validation matrix
     test_matrix = dataset['test-matrix'].tocsr() # compressed sparse row test matrix
     item_texts = dataset['item-texts'] # item text features
+    # imgset = dataset['item-images'] # item image features
     user_ntype = dataset['user-type'] # user node
     item_ntype = dataset['item-type'] # item node
     user_to_item_etype = dataset['user-to-item-type'] # user-item directed edge
     timestamp = dataset['timestamp-edge-column'] # timestamp column
 
-    device = torch.device(args.device)
+    device = torch.device('cuda') if torch.cuda.is_available() else torch.device('cpu')
 
-    # Assign user and movie IDs and use them as features (to learn an individual trainable
-    # embedding for each entity)
+    # device = torch.device(model_cfg['device'])
+
+    if device.type == 'cpu':
+        print('Using CPU...')
+    else:
+        print("Using CUDA: {}...".format(torch.cuda.current_device()))
+
+    # Assign user and item IDs and use them as features
+    # to learn an individual trainable embedding for each entity
     g.nodes[user_ntype].data['id'] = torch.arange(g.number_of_nodes(user_ntype))
     g.nodes[item_ntype].data['id'] = torch.arange(g.number_of_nodes(item_ntype))
 
+    # Text Features
     # Prepare torchtext dataset and vocabulary
-    fields = {}
-    examples = []
-    for key, texts in item_texts.items():
-        fields[key] = torchtext.data.Field(include_lengths=True, lower=True, batch_first=True)
-    for i in range(g.number_of_nodes(item_ntype)):
-        example = torchtext.data.Example.fromlist(
-            [item_texts[key][i] for key in item_texts.keys()],
-            [(key, fields[key]) for key in item_texts.keys()])
-        examples.append(example)
-    textset = torchtext.data.Dataset(examples, fields)
-    for key, field in fields.items():
-        field.build_vocab(getattr(textset, key))
-        #field.build_vocab(getattr(textset, key), vectors='fasttext.simple.300d') # use fasttext
+    if item_texts is not None:
+        fields = {}
+        examples = []
+        for key, texts in item_texts.items():
+            fields[key] = torchtext.data.Field(include_lengths=True, lower=True, batch_first=True)
+        for i in range(g.number_of_nodes(item_ntype)):
+            example = torchtext.data.Example.fromlist(
+                [item_texts[key][i] for key in item_texts.keys()],
+                [(key, fields[key]) for key in item_texts.keys()])
+            examples.append(example)
+
+        textset = torchtext.data.Dataset(examples, fields)
+        for key, field in fields.items():
+            field.build_vocab(getattr(textset, key))
+            #field.build_vocab(getattr(textset, key), vectors='fasttext.simple.300d') # use fasttext
+
+    # Image Features
+    # g.nodes[item_ntype].data['image'] = torch.FloatTensor(item_images['image'])
 
     # Batch Sampler
     batch_sampler = sampler_module.ItemToItemBatchSampler(
-        g, user_ntype, item_ntype, args.batch_size)
+        g, user_ntype, item_ntype, model_cfg['batch_size'])
 
     # Neighbor Sampler
     neighbor_sampler = sampler_module.NeighborSampler(
-        g, user_ntype, item_ntype, args.random_walk_length,
-        args.random_walk_restart_prob, args.num_random_walks, args.num_neighbors,
-        args.num_layers)
+        g, user_ntype, item_ntype,
+        model_cfg['random_walk_length'], model_cfg['random_walk_restart_prob'],
+        model_cfg['num_random_walks'], model_cfg['num_neighbors'], model_cfg['num_layers'])
 
     # Collator
     collator = sampler_module.PinSAGECollator(neighbor_sampler, g, item_ntype, textset)
 
-    # Train Data Loader
+    # Training Data Loader
     dataloader = DataLoader(
         batch_sampler,
         collate_fn=collator.collate_train,
-        num_workers=args.num_workers)
+        num_workers=model_cfg['num_workers'])
 
     # Test Data Loader
     dataloader_test = DataLoader(
         torch.arange(g.number_of_nodes(item_ntype)),
-        batch_size=args.batch_size,
+        batch_size=model_cfg['batch_size'],
         collate_fn=collator.collate_test,
-        num_workers=args.num_workers)
+        num_workers=model_cfg['num_workers'])
+
+    # training Data Loader Iterator
     dataloader_it = iter(dataloader)
 
     # Model
-    model = PinSAGEModel(g, item_ntype, textset, args.hidden_dims, args.num_layers).to(device)
+    model = PinSAGEModel(g, item_ntype, textset, model_cfg['hidden_dims'], model_cfg['num_layers']).to(device)
+    opt = torch.optim.Adam(model.parameters(), lr=model_cfg['lr'])
+    start_epoch = 0
 
-    # Optimizer
-    opt = torch.optim.Adam(model.parameters(), lr=args.lr)
+    # load existing model if exists
+    if model_cfg['existing-model'] is not None:
+        state = torch.load(
+            os.path.join(model_cfg['model-dir'], model_cfg['existing-model']), 
+            map_location=device
+        )
+        model.load_state_dict(state['model_state_dict'])
+        opt.load_state_dict(state['optimizer_state_dict'])
+        start_epoch = state['epoch']
 
     # For each batch of head-tail-negative triplets...
-    for epoch_id in range(args.num_epochs):
+    for epoch_id in range(start_epoch, model_cfg['num_epochs'] + start_epoch):
 
         # Train
         model.train()
-        for batch_id in tqdm.trange(args.batches_per_epoch):
+        for batch_id in tqdm(range(model_cfg['batches_per_epoch'])):
+
+            # get next batch of training data
             pos_graph, neg_graph, blocks = next(dataloader_it)
 
             # Copy to GPU
@@ -138,7 +174,7 @@ def train(dataset, args):
             neg_graph = neg_graph.to(device)
 
             # Calculate loss
-            loss = model(pos_graph, neg_graph, blocks).mean() # mean
+            loss = model(pos_graph, neg_graph, blocks).mean()
 
             # Zero optimizer gradients
             opt.zero_grad()
@@ -146,55 +182,80 @@ def train(dataset, args):
             # Backpropagate loss
             loss.backward()
 
-            # Adjust optimizer weights
+            # Adjust model weights
             opt.step()
 
-        # Evaluate validation set
+            print("Training: epoch: {}, batch: {}, loss: {}".format(epoch_id, batch_id, loss))
+
+        # Evaluate validation set after training epoch
         model.eval()
         with torch.no_grad():
-            item_batches = torch.arange(g.number_of_nodes(item_ntype)).split(args.batch_size)
+            # item batches are groups of node numbers
+            item_batches = torch.arange(g.number_of_nodes(item_ntype)).split(model_cfg['batch_size'])
             h_item_batches = []
+            # use test dataloader to get sampled neighbors
             for blocks in dataloader_test:
+                # move blocks to GPU
                 for i in range(len(blocks)):
                     blocks[i] = blocks[i].to(device)
+                # get embedding of blocks
                 h_item_batches.append(model.get_repr(blocks))
-            h_item = torch.cat(h_item_batches, 0) # item node embeddings
-            print(evaluation.evaluate_nn(dataset, h_item, args.k, args.batch_size))
-    return model
 
-def test(dataset, args):
-    model.eval()
-    with torch.no_grad():
-        item_batches = torch.arange(g.number_of_nodes(item_ntype)).split(args.batch_size)
-        h_item_batches = []
-        for blocks in dataloader_test:
-            for i in range(len(blocks)):
-                blocks[i] = blocks[i].to(device)
-            h_item_batches.append(model.get_repr(blocks))
-        h_item = torch.cat(h_item_batches, 0) # item node embeddings
-        print(evaluation.evaluate_nn(dataset, h_item, args.k, args.batch_size))
+            # concatenate all embeddings of the batch
+            h_item = torch.cat(h_item_batches, 0) # item node embeddings
+
+            # calculate model evaluation metrics
+            hit, precision, recall = evaluation.evaluate(
+                dataset, h_item, model_cfg['k'], model_cfg['batch_size'])
+
+            print("Evaluation @ {}: hit: {}, precision: {}, recall: {}"
+                  .format(model_cfg['k'], hit, precision, recall))
+
+        # save model after every epoch
+        state = {
+            'epoch': epoch_id+1,
+            'model_state_dict': model.state_dict(),
+            'optimizer_state_dict': opt.state_dict(),
+            'loss': epoch_loss,
+            'item_embeddings': h_item,
+            'k': model_cfg['k'],
+            'batch_size': model_cfg['batch_size']
+        }
+        torch.save(state, "{}_model_{}.pth".format(model_cfg['name'], epoch_id))
+
+    return model, opt, h_item
 
 
 if __name__ == '__main__':
     # Arguments
-    parser = argparse.ArgumentParser()
-    parser.add_argument('dataset_path', type=str) # path to dataset pickle file
-    parser.add_argument('--random-walk-length', type=int, default=2)
-    parser.add_argument('--random-walk-restart-prob', type=float, default=0.5)
-    parser.add_argument('--num-random-walks', type=int, default=10)
-    parser.add_argument('--num-neighbors', type=int, default=3)
-    parser.add_argument('--num-layers', type=int, default=2)
-    parser.add_argument('--hidden-dims', type=int, default=16)
-    parser.add_argument('--batch-size', type=int, default=32)
-    parser.add_argument('--device', type=str, default='cpu') # can also be "cuda:0"
-    parser.add_argument('--num-epochs', type=int, default=1)
-    parser.add_argument('--batches-per-epoch', type=int, default=20000)
-    parser.add_argument('--num-workers', type=int, default=0) # dask workers
-    parser.add_argument('--lr', type=float, default=3e-5)
-    parser.add_argument('-k', type=int, default=10) # number of neighbors to recommend
-    args = parser.parse_args()
+    # parser = argparse.ArgumentParser()
+    # parser.add_argument('dataset_path', type=str) # path to dataset pickle file
+    # parser.add_argument('--random-walk-length', type=int, default=2)
+    # parser.add_argument('--random-walk-restart-prob', type=float, default=0.5)
+    # parser.add_argument('--num-random-walks', type=int, default=10)
+    # parser.add_argument('--num-neighbors', type=int, default=3)
+    # parser.add_argument('--num-layers', type=int, default=2)
+    # parser.add_argument('--hidden-dims', type=int, default=16)
+    # parser.add_argument('--batch-size', type=int, default=32)
+    # parser.add_argument('--device', type=str, default='cpu') # can also be "cuda:0"
+    # parser.add_argument('--num-epochs', type=int, default=1)
+    # parser.add_argument('--batches-per-epoch', type=int, default=20000)
+    # parser.add_argument('--num-workers', type=int, default=0) # dask workers
+    # parser.add_argument('--lr', type=float, default=3e-5)
+    # parser.add_argument('-k', type=int, default=10) # number of neighbors to recommend
+    # args = parser.parse_args()
 
     # Load dataset
-    with open(args.dataset_path, 'rb') as f:
+    data_dir = "../../data"
+    dataset_fn = "processed_Amazon_Electronics.pkl"
+    with open(os.path.join(data_dir, dataset_fn), 'rb') as f:
         dataset = pickle.load(f)
-    train(dataset, args)
+
+    # Load config
+    config_dir = "../../config"
+    config_fn = "pinsage-model-params.json"
+    with open(os.path.join(config_dir, config_fn)) as fh:
+        model_config = json.load(fh)
+
+    print("beginning training")
+    train(dataset, model_config)
